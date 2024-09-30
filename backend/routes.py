04ddfd8c53b09a521291e28a3820ebb6a8540ad1
Tasneem849 <tasneemmohamed2013@gmail.com>
2024-09-30 17:25:09 +0300

    Push1

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,10 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+    
+    return {"message": "Internal server error"}, 500
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +47,11 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    if data:
+        for img in data:
+            if img['id'] == id:
+                return jsonify(img), 200
+    return {"message": "Picture not found"}, 404
 
 
 ######################################################################
@@ -52,7 +59,15 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    newImage = request.json
+    
+    for img in data:
+        if img['id'] == newImage['id']:
+            return {"Message": f"picture with id {newImage['id']} already present"}, 302
+    
+    data.append(newImage)
+    
+    return jsonify(newImage), 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +76,23 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    newPictureData = request.json
+    
+    for index, img in enumerate(data):
+        if img['id'] == id:
+            data[index] = newPictureData
+            return jsonify(newPictureData), 201
+
+    return {"message": "picture not found"}, 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
-def delete_picture(id):
-    pass
+def delete_picture(id):   
+    for index, img in enumerate(data):
+        if img['id'] == id:
+            del data[index]
+            return '', 204
+            
+    return {"message": "picture not found"}, 404
